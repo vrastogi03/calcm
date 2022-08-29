a3f8775f0ae8c8939668fea725c9be56901545f2
vrastogi03 <rastogivibhu1999@gmail.com>
2022-08-29 12:13:46 +0530

    first commit

---
+++ src/main/java/com/vibhu/calcm/CalcmApplication.java
@@ -0,0 +1,13 @@
+package com.vibhu.calcm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vibhu/calcm/CalcmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vibhu.calcm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
